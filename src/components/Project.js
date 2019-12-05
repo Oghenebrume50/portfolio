72862781989061875cda3243a608a89e951544e3
Oghenebrume50 <raphlbrume@gmail.com>
2019-12-05 18:40:07 +0100

    made project title and desc overlay on image

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,15 +5,15 @@ class Project extends React.Component {
   render() {
     const {img_src, proj_title, proj_desc} = this.props;
     return (
-      <div className="a-project">
+      <a className="a-project" href="https://github.com/Oghenebrume50/portfolio">
         <div>
           <img src={img_src} alt="bookstore"/>
         </div>
-        <div>
+        <div className="a-project-footer">
           <p className="project_title">{proj_title}</p>
           <p className="project_desc">{proj_desc}</p>
         </div>
-      </div>
+      </a>
     )
   }
 }
--- src/containers/Works.js
+++ src/containers/Works.js
@@ -13,9 +13,29 @@ class Work extends React.Component {
             proj_title="BookStore"
             proj_desc="A project to store books owned and read by an individual"
             />
-          <Project />
-          <Project />
-          <Project />
+          <Project 
+            img_src="https://res.cloudinary.com/raphaelnoriode/image/upload/v1575559074/portfolio%20pic/photo-1522199670076-2852f80289c3_dciyxp.jpg"
+            proj_title="Todo list"
+            proj_desc="A todo list app for task management"
+          />
+
+          <Project 
+            img_src="https://res.cloudinary.com/raphaelnoriode/image/upload/v1575559074/portfolio%20pic/11_vpkfgg.png"
+            proj_title="Battleship"
+            proj_desc="A battleship game with player and CPU"
+          />
+
+          <Project 
+            img_src="https://res.cloudinary.com/raphaelnoriode/image/upload/v1575559074/portfolio%20pic/photo-1432888622747-4eb9a8efeb07_xikkfx.png"
+            proj_title="A facebook clone"
+            proj_desc="cloning the basic functionality of a social app"
+          />
+
+          <Project
+            img_src="https://res.cloudinary.com/raphaelnoriode/image/upload/v1575559073/portfolio%20pic/photo-1501139083538-0139583c060f_ipbpon.jpg"
+            proj_title="Chronos"
+            proj_desc="A discord timer bot, to remind users of previous messages"
+          />
         </div>
       </div>
     )
