9056257839204f40cc4fd3dbec9da7e8096de968
oghenebrume50 <raphlbrume@gmail.com>
2020-04-18 17:52:56 +0100

    setup google analytics using reatc-ga

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
+
+const trackingId = process.env.REACT_APP_TRACK_ID;
+ReactGA.initialize(trackingId);
+ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
